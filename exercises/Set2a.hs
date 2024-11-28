03eb3d50b9c6fe652e65a4349da762c4a8628fed
xssxrt <179648286+xssxrt@users.noreply.github.com>
2024-11-27 22:28:14 -0500

    set2a

--- exercises/Set2a.hs
+++ exercises/Set2a.hs
@@ -22,6 +22,7 @@ import Data.List
 -- 2004 and 2020 in this order.
 
 years = [1982, 2004, 2020]
+
 ------------------------------------------------------------------------------
 -- Ex 2: define the function takeFinal, which returns the n last
 -- elements of the given list.
@@ -44,8 +45,12 @@ takeFinal n xs = drop (length(xs) - n) xs
 --   updateAt 0 4 [1,2,3]   ==>  [4,2,3]
 --   updateAt 2 0 [4,5,6,7] ==>  [4,5,0,7]
 
+-- splitAt gets us pretty much there we just need to drop the 
+-- the first element in the snd of the tuple
 updateAt :: Int -> a -> [a] -> [a]
-updateAt i x xs = xs
+updateAt i x xs = let (before, after) = splitAt i xs 
+    in 
+        before ++ (x : tail after)
 
 ------------------------------------------------------------------------------
 -- Ex 4: substring i j s should return the substring of s starting at
@@ -59,7 +64,7 @@ updateAt i x xs = xs
 --   substring 0 4 "abcdefgh"  ==>  "abcd"
 
 substring :: Int -> Int -> String -> String
-substring i j s = todo
+substring i j s = drop i (take j s)
 
 ------------------------------------------------------------------------------
 -- Ex 5: check if a string is a palindrome. A palindrome is a string
@@ -74,7 +79,7 @@ substring i j s = todo
 --   isPalindrome "AB"       ==>  False
 
 isPalindrome :: String -> Bool
-isPalindrome str = todo
+isPalindrome str = str == reverse str
 
 ------------------------------------------------------------------------------
 -- Ex 6: implement the function palindromify that chops a character
@@ -88,7 +93,9 @@ isPalindrome str = todo
 --   palindromify "abracacabra" ==> "acaca"
 
 palindromify :: String -> String
-palindromify s = todo
+palindromify s
+  | isPalindrome s = s
+  | otherwise = palindromify (tail (init s))
 
 ------------------------------------------------------------------------------
 -- Ex 7: implement safe integer division, that is, a function that
@@ -101,7 +108,9 @@ palindromify s = todo
 --   safeDiv 4 0  ==> Nothing
 
 safeDiv :: Integer -> Integer -> Maybe Integer
-safeDiv x y = todo
+safeDiv x y 
+  | y == 0 = Nothing
+  | otherwise = Just (div x y)
 
 ------------------------------------------------------------------------------
 -- Ex 8: implement a function greet that greets a person given a first
@@ -113,7 +122,8 @@ safeDiv x y = todo
 --   greet "John" (Just "Smith")  ==> "Hello, John Smith!"
 
 greet :: String -> Maybe String -> String
-greet first last = todo
+greet first Nothing = "Hello, " ++ first ++ "!"
+greet first (Just last) = "Hello, " ++ first ++ " " ++ last ++ "!"
 
 ------------------------------------------------------------------------------
 -- Ex 9: safe list indexing. Define a function safeIndex so that
@@ -129,7 +139,9 @@ greet first last = todo
 --   safeIndex ["a","b","c"] (-1)  ==> Nothing
 
 safeIndex :: [a] -> Int -> Maybe a
-safeIndex xs i = todo
+safeIndex xs i
+    | i < 0 || i >= length(xs) = Nothing
+    | otherwise = Just (xs !! i)
 
 ------------------------------------------------------------------------------
 -- Ex 10: another variant of safe division. This time you should use
@@ -140,7 +152,9 @@ safeIndex xs i = todo
 --   eitherDiv 4 0   ==> Left "4/0"
 
 eitherDiv :: Integer -> Integer -> Either String Integer
-eitherDiv x y = todo
+eitherDiv x y
+  | y == 0 = Left(show x ++ "/0")
+  | otherwise = Right (div x y)
 
 ------------------------------------------------------------------------------
 -- Ex 11: implement the function addEithers, which combines two values of type
@@ -157,5 +171,7 @@ eitherDiv x y = todo
 --   addEithers (Left "boom") (Left "fail") ==> Left "boom"
 
 addEithers :: Either String Int -> Either String Int -> Either String Int
-addEithers a b = todo
+addEithers (Right a) (Right b) = (Right (a+b))
+addEithers (Left a) _ = Left a
+addEithers _ (Left b) = Left b
 
